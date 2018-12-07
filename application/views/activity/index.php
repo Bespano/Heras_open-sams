8c09e4c8b88140ffc614e83a8fd0fb3725b5cc64
Bespano <bienvenido.espinosa@gmail.com>
2018-12-07 11:12:27 +0100

    success text message with CSS

--- application/views/activity/index.php
+++ application/views/activity/index.php
@@ -46,13 +46,17 @@
   <div class="page-header">
     <h2>Registro de Actividad</h2>
   </div>
-   <?php echo $success; if (isset($success)){ ?>
+   <?php echo $success; 
+    if (isset($success)){ ?>
     
-    <div class="alert alert-success" role="alert">
-      <strong>Correcto</strong> <?php   echo (isset($success)) ? $success : "";?>
-  </div>
-  <?php 
-  }else{echo "no success";}?>
+        <div class="alert alert-success" role="alert">
+          <strong>Correcto</strong> <?php   echo (isset($success)) ? $success : "";?>
+      </div>
+  <?php       
+    }else{?>
+      <div class="alert alert-info" role="alert"> "Sin cambios"</div>
+      
+   <?php }?>
  
   <div class="row">
       <div class="pull-right">
